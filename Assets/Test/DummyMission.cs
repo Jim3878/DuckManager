7307f05afae9b52c6630b0811a8739beb6efd762
jim3878 <jim3878@gmail.com>
2018-03-27 18:28:54 +0800

    補測試
    補上是否會Wait的測試

---
+++ Assets/Test/DummyMission.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DuckManager
+{
+    public class DummyMission : BaseMission
+    {
+        public int targetUpdate;
+        public int updateCount;
+        public DummyMission(int targetUpdate)
+        {
+            this.targetUpdate = targetUpdate;
+            this.updateCount = 0;
+        }
+        public override void StateUpdate()
+        {
+            TransToNext();
+            updateCount++;
+            if (updateCount >= targetUpdate)
+            {
+                TransToNext();
+            }
+
+        }
+    }
+}
---
+++ Assets/Test/Editor/DuckControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using DuckManager;
+public class DuckControllerTest {
+    
+
+    [Test]
+	public void WhenMissionQueeIsEmpty_WillTransToWaitState() {
+        // Use the Assert class to test conditions.
+        DuckController duck = new DuckController(0);
+
+        Assert.IsTrue(duck.currentState is WaitState);//初始化後預設狀態為waitState;
+
+        duck.AddMission(new DummyMission(0));
+        duck.Update();//退出Wait
+        duck.Update();//退出dummyMission
+
+        Assert.IsTrue(duck.currentState is WaitState);//dummyMission結束後轉入 waitState;
+    }
+    
+}
